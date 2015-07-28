d34f453fe0587e1f637aa20ae10ae092bb46204d
proguen <pete@uen.org>
2015-07-28 05:02:14 -0600

    setup improvements

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 1. Install Ubuntu 14.04 64-bit - optionally install the OpenSSH package
 
-2. After Ubuntu is installed, run the Weathermap setup script
+2. After Ubuntu is installed, log in and run the Weathermap setup script
 
-    `$] wget -q -O - https://raw.githubusercontent.com/proguen/weathermap/master/install/setup.sh | sudo bash`
+    `]$ perl <(wget -q -O - https://raw.githubusercontent.com/proguen/weathermap/master/install/setup.pl)
 
 3. When the script completes, edit `/opt/weathermap/conf/config.js` to set the default SNMP community for your devices
 
--- install/setup.pl
+++ install/setup.pl
@@ -1,23 +1,73 @@
 #!/usr/bin/perl
-print "\n\nUpdating Ubuntu ... ";
-system("apt-get -y update >/dev/null && sudo apt-get -y upgrade >/dev/null && sudo aptitude -y safe-upgrade >/dev/null");
+$| = 1;
+
+run_command("sudo true"); # Prompt for sudo password right away
+
+print "\n\nSetting up Weathermap\n";
+print "\nUpdating Ubuntu\n  apt-get update ";
+run_command("sudo apt-get -y update", 1);
+print " Done\n  apt-get upgrade ";
+run_command("sudo apt-get -y upgrade", 1);
+print " Done\n  aptitude safe-upgrade ";
+run_command("sudo aptitude -y safe-upgrade", 1);
 print " Done\n";
 
-print "\nInstalling required packages ... ";
-system("apt-get install -y git libcommon-sense-perl libdbix-simple-perl libnet-snmp-perl npm mysql-client >/dev/null");
+print "\nInstalling required packages\n";
+for (qw/git libcommon-sense-perl libdbix-simple-perl libnet-snmp-perl npm mysql-client/) {
+    print "  $_ \n";
+    run_command("sudo apt-get install -y $_ >/dev/null");
+}
 print " Done\n";
 
-print "\nInstalling MySQL -- leave root passwords blank when prompted\n";
-print "Press <ENTER> to continue\n";
-<STDIN>;
-system("apt-get install -y mysql-server");
+print "\nInstalling MySQL server -- \n ** leave MySQL root password blank - just press Enter or Return when prompted for a MySQL root password **\n";
+print "Press any key to continue\n";
+keypress();
+run_command("sudo apt-get install -y mysql-server");
+print "\n  Done installing MySQL server\n";
 
-print "\nInstalling Weathermap ... ";
-system("git clone https://github.com/proguen/weathermap.git /opt/weathermap");
-system("mysqladmin -u root create snmp");
-system("mysql -u root snmp < /opt/weathermap/install/sql.ddl");
-system("cd /opt/weathermap && sudo npm install");
-system("cp /opt/weathermap/install/weathermap-app.conf /opt/weathermap/install/weathermap-poller.conf /opt/weathermap/install/weathermap-updater.conf /etc/init");
-print " Done\n";
+print "\nInstalling Weathermap\n";
+if (-e "/opt/weathermap" && -e "/opt/weathermap/.git") {
+    print "  Updating GitHub repo .. ";
+    run_command("cd /opt/weathermap; sudo git pull >/dev/null");
+} else {
+    print "  Cloning GitHub repo .. ";
+    run_command("sudo git clone https://github.com/proguen/weathermap.git /opt/weathermap >/dev/null");
+}
+print "Done\n";
+if(system("echo 'select 1' | mysql -u root snmp >/dev/null 2>&1")) {
+    print "  Setting up MySQL database .. ";
+    run_command("sudo mysqladmin -u root create snmp >/dev/null");
+    run_command("sudo mysql -u root snmp < /opt/weathermap/install/sql.ddl >/dev/null");
+    print "Done\n";
+}
+print "  Finalizing setup .. ";
+run_command("cd /opt/weathermap && sudo npm install >/dev/null 2>&1");
+run_command("sudo cp /opt/weathermap/install/weathermap-app.conf /opt/weathermap/install/weathermap-poller.conf /opt/weathermap/install/weathermap-updater.conf /etc/init >/dev/null");
+print "Done\n";
 
 print "\n\n ** Edit /opt/weathermap/conf/config.js, then restart **\n\n";
+
+sub run_command {
+    my ($command, $show_progress) = @_;
+    if ($show_progress) {
+	open my $cmd, "$command |";
+	while(<$cmd>) {
+	    print ".";
+	}
+	close $output;
+    } else {
+	if (system($command)) {
+	    print "\n Setup process failed\n";
+	    exit;
+	}
+    }
+}
+
+sub keypress {
+    require Term::ReadKey;
+    my $key;
+    Term::ReadKey::ReadMode(4); # Turn off controls keys
+    while (!defined ($key = Term::ReadKey::ReadKey(-1))) { }
+    Term::ReadKey::ReadMode(0); # Reset tty mode before exiting
+    die "Break pressed" if ord($key) == 3;
+}
